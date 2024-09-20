14161150b7b1c6b0f59d353c7949a16783c53725
akemi-adam <bazilixo8@gmail.com>
2024-09-20 17:49:14 -0300

    new: versão da função selectOption para Windows

--- src/interfaces.c
+++ src/interfaces.c
@@ -1,10 +1,13 @@
 #include "interfaces.h"
 #include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#ifdef __unix__
+
+#include <termios.h>
+#include <unistd.h>
 
 /*
  * Desabilita o modo canônico e habilita o raw mode (modo bruto) do terminal
@@ -49,7 +52,7 @@ void disableRawMode(struct termios *originalTerminal) {
 }
 
 /*
- * Incrementa ou decrementa um inteiro ao pressionar as teclas de Down-Arrow ou Up-Arrow. Enter é interpretado como a seleção de uma opção.
+ * Incrementa ou decrementa um inteiro ao pressionar as teclas de Down-Arrow (B) ou Up-Arrow (A). Enter é interpretado como a seleção de uma opção.
  * 
  * Inputs:
  *  - int *option: Um ponteiro de inteiro que representa a opção escolhida no menu
@@ -83,6 +86,29 @@ void selectOption(int *option, int optionsAmount, bool *isSelected) {
         }
     }
 }
+
+#else
+
+#include <conio.h>
+
+void selectOption(int *option, int optionsAmount, bool *isSelected) {
+    int ch = getch();
+    if (ch == 13) {
+        *isSelected = true;
+    } else if (ch == 0 || ch = 224) {
+        ch = getch();
+        switch (ch) {
+            case 72:
+                *option = (*option > 0) ? *option - 1 : optionsAmount;
+                break;
+            case 80:
+                *option = (*option < optionsAmount) ? *option + 1 : 0;
+                break;
+        }
+    }
+}
+
+#endif
 
 void setOptionsStyle(char optionsStyles[][30], int size) {
     strcpy(optionsStyles[0], CYAN_UNDERLINE_TEXT);
--- src/interfaces.h
+++ src/interfaces.h
@@ -1,17 +1,20 @@
 #ifndef INTERFACES
 #define INTERFACES
 
-#ifdef __unix__
-    #include <termios.h>
-#endif
 #include <stdbool.h>
 
 #define CYAN_UNDERLINE_TEXT "\e[4;36m"
 #define RESET_STYLE "\e[0m"
 
+#ifdef __unix__
+
+#include <termios.h>
+
 void enableRawMode(void);
 
 void disableRawMode(struct termios*);
+
+#endif
 
 void selectOption(int*, int, bool*);
 
