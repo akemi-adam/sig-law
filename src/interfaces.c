49f0f761552dc3a8d38454588bcc8861a7575dc7
Felipe Erik <felipeerik1524@gmail.com>
2024-09-23 11:44:58 -0300

    new: implementação da função showLawyerMenu

--- src/interfaces.c
+++ src/interfaces.c
@@ -288,7 +288,67 @@ void showClientMenu() {
 }
 
 void showLawyerMenu() {
-    //
+    #ifdef _unix_
+        struct termios originalTerminal;
+        tcgetattr(STDIN_FILENO, &originalTerminal);
+        enableRawMode();
+    #endif
+    int size = 5;
+    char optionsStyles[size][11];
+    setOptionsStyle(optionsStyles, size);
+
+    int option = 0, aux;
+    bool isSelected = false;
+    bool loop = true;
+    while (loop) {
+        #ifdef _unix_
+            system("clear");
+        #else
+            system("cls");
+        #endif
+        if (!isSelected) {
+            printf("----- Menu Advogado -----\n");
+            printf("|                       |\n");
+            printf("| %s1. Cadastar advogado%s  |\n", optionsStyles[0], RESET_STYLE);
+            printf("| %s2. Mostar advogado%s    |\n", optionsStyles[1], RESET_STYLE);
+            printf("| %s3. Editar advogado%s    |\n", optionsStyles[2], RESET_STYLE);
+            printf("| %s4. Exluir advogado%s    |\n", optionsStyles[3], RESET_STYLE);
+            printf("| %s5. Voltar ao menu%s     |\n", optionsStyles[4], RESET_STYLE);
+            printf("|                       |\n");
+            printf("-------------------------\n");
+            
+            aux = option;
+            selectOption(&option, size - 1, &isSelected);
+
+            strcpy(optionsStyles[aux], RESET_STYLE);
+            strcpy(optionsStyles[option], CYAN_UNDERLINE_TEXT);
+        } else {
+            #ifdef _unix_
+                disableRawMode(&originalTerminal);
+            #endif
+            isSelected = false;
+            switch (option) {
+                case 0:
+                    //createLawyer();
+                    break;
+                case 1:
+                    //readLawyer();
+                    break;
+                case 2:
+                    //updateLawyer();
+                    break;
+                case 3:
+                    //deleteLawyer();
+                    break;
+                default:
+                    loop = false;
+                    break;
+            }
+            #ifdef _unix_
+                enableRawMode();
+            #endif
+        }
+    }
 }
 
 void showOfficeMenu() {
