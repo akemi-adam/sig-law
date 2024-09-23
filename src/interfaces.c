b48ba648de7c231442c01b7cf155547fc7e8ac8a
Felipe Erik <felipeerik1524@gmail.com>
2024-09-23 11:39:59 -0300

    new: implementação da função showClientMenu

--- src/interfaces.c
+++ src/interfaces.c
@@ -224,7 +224,67 @@ void showMainMenu() {
 }
 
 void showClientMenu() {
-    //
+    #ifdef __unix__
+        struct termios originalTerminal;
+        tcgetattr(STDIN_FILENO, &originalTerminal);
+        enableRawMode();
+    #endif
+    int size = 5;
+    char optionsStyles[size][11];
+    setOptionsStyle(optionsStyles, size);
+
+    int option = 0, aux;
+    bool isSelected = false;
+    bool loop = true;
+    while (loop) {
+        #ifdef __unix__
+            system("clear");
+        #else
+            system("cls");
+        #endif
+        if (!isSelected) {
+            printf("----- Menu Cliente -----\n");
+            printf("|                      |\n");
+            printf("| %s1. Cadastar cliente%s  |\n", optionsStyles[0], RESET_STYLE);
+            printf("| %s2. Mostar cliente%s    |\n", optionsStyles[1], RESET_STYLE);
+            printf("| %s3. Editar cliente%s    |\n", optionsStyles[2], RESET_STYLE);
+            printf("| %s4. Exluir cliente%s    |\n", optionsStyles[3], RESET_STYLE);
+            printf("| %s5. Voltar ao menu%s    |\n", optionsStyles[4], RESET_STYLE);
+            printf("|                      |\n");
+            printf("------------------------\n");
+            
+            aux = option;
+            selectOption(&option, size - 1, &isSelected);
+
+            strcpy(optionsStyles[aux], RESET_STYLE);
+            strcpy(optionsStyles[option], CYAN_UNDERLINE_TEXT);
+        } else {
+            #ifdef __unix__
+                disableRawMode(&originalTerminal);
+            #endif
+            isSelected = false;
+            switch (option) {
+                case 0:
+                    //createClient();
+                    break;
+                case 1:
+                    //readClient();
+                    break;
+                case 2:
+                    //updateClient();
+                    break;
+                case 3:
+                    //deleteClient();
+                    break;
+                default:
+                    loop = false;
+                    break;
+            }
+            #ifdef __unix__
+                enableRawMode();
+            #endif
+        }
+    }
 }
 
 void showLawyerMenu() {
