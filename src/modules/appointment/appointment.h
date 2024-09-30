ca7ab385de3b3a24c6358709391e0d692f97f618
akemi-adam <bazilixo8@gmail.com>
2024-09-30 15:55:07 -0300

    new: Definição da struct Schedule e Appointment

--- src/modules/appointment/appointment.h
+++ src/modules/appointment/appointment.h
@@ -1,4 +1,18 @@
 #ifndef APPOINTMENT
 #define APPOINTMENT
 
+#include "./../../utils/date.h"
+
+struct Schedule {
+    Datetime *startDate;
+    Datetime *endDate;
+};
+
+struct Appointment {
+    int clientId;
+    int lawyerId;
+    int officeId;
+    Schedule *schedule;
+};
+
 #endif
