6da64ee4171c74c17894c0a9e58744b32b3549ff
akemi-adam <bazilixo8@gmail.com>
2024-10-01 20:28:43 -0300

    new: Formulário para cadastrar agendamento

--- src/modules/appointment/usecases.c
+++ src/modules/appointment/usecases.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include "./../../utils/interfaces.h"
+#include "./../../utils/date.h"
+#include "appointment.h"
 
 #ifdef __unix__
 
@@ -11,6 +13,42 @@
 #include <unistd.h>
 
 #endif
+
+void createAppointment() {
+    Appointment *appointment = malloc(sizeof(Appointment));
+    appointment->schedule = malloc(sizeof(Schedule));
+    appointment->schedule->endDate = malloc(sizeof(Datetime));
+    appointment->schedule->startDate = malloc(sizeof(Datetime));
+    char date[11], startTime[6], endTime[6];
+
+    printf("---- Cadastrar Advogado ----\n");
+    printf("Código do Cliente: ");
+    scanf("%d", &appointment->clientId);
+
+    printf("Código do Advogado: ");
+    scanf("%d", &appointment->lawyerId);
+
+    printf("Código do Escritório: ");
+    scanf("%d", &appointment->officeId);
+    flushInput();
+
+    printf("Data (dd/mm/aaaa): ");
+    readline(date, 11);
+
+    printf("Horário do início da consulta (hh:mm): ");
+    readline(startTime, 11);
+
+    printf("Horário do término da consulta (hh:mm): ");
+    readline(endTime, 11);
+
+    free(appointment->schedule->startDate);
+    free(appointment->schedule->endDate);
+    free(appointment->schedule);
+    free(appointment);
+
+    printf("\nUsuário cadastrado com sucesso!\nPressione <Enter> para prosseguir...\n");
+    proceed();
+}
 
 /**
  * Exibe o menu do módulo agendamento e que pede para o usuário selecionar uma opção
@@ -57,7 +95,6 @@ void showAppointmentMenu() {
                 actions[option]();
             } else {
                 loop = false;
-                    break;
             }
         }
     }
