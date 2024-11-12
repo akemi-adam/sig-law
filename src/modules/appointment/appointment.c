a92b699a8d991686ebf2aca0fb35868afd1e497d
akemi-adam <bazilixo8@gmail.com>
2024-11-11 23:11:47 -0300

    new: Validação no módulo appointment

--- src/modules/appointment/appointment.c
+++ src/modules/appointment/appointment.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "./../../utils/interfaces.h"
+#include "./../../utils/validation.h"
 #include "./../../utils/date.h"
 #include "appointment.h"
 
@@ -23,27 +24,19 @@
  */
 void createAppointment() {
     Appointment appointment;
-    char date[11], startTime[6], endTime[6];
+    char date[11], startTime[6], endTime[6], clientId[6], lawyerId[6], officeId[6];
 
+    Validation idRules[3] = {validateRequired, validateNumber, validatePositive},
+        dateRules[2] = {validateRequired, validateDate},
+        hourRules[2] = {validateRequired, validateHour};
+        
     printf("---- Cadastrar Agendamento ----\n");
-    printf("Código do Cliente: ");
-    scanf("%d", &appointment.clientId);
-
-    printf("Código do Advogado: ");
-    scanf("%d", &appointment.lawyerId);
-
-    printf("Código do Escritório: ");
-    scanf("%d", &appointment.officeId);
-    flushInput();
-
-    printf("Data (dd/mm/aaaa): ");
-    readline(date, 11);
-
-    printf("Horário do início da consulta (hh:mm): ");
-    readline(startTime, 11);
-
-    printf("Horário do término da consulta (hh:mm): ");
-    readline(endTime, 11);
+    readStrField(clientId, "Código do Cliente", 6, idRules, 3);
+    readStrField(lawyerId, "Código do Advogado", 6, idRules, 3);
+    readStrField(officeId, "Código do Escritório", 6, idRules, 3);
+    readStrField(date, "Data (dd/mm/aaaa)", 11, dateRules, 2);
+    readStrField(startTime, "Horário do início da consulta (hh:mm)", 6, hourRules, 2);
+    readStrField(endTime, "Horário do término da consulta (hh:mm)", 6, hourRules, 2);
 
     printf("\nAgendamento cadastrado com sucesso!\nPressione <Enter> para prosseguir...\n");
     proceed();
@@ -75,11 +68,12 @@ void listAppointments() {
  *  - https://github.com/akemi-adam
  */
 void readAppointment() {
-    int id;
-    printf("---- Buscar Agendamento ----\nCódigo do Agendamento: ");
-    scanf("%d", &id);
+    char id[6];
+    Validation idRules[3] = {validateRequired, validateNumber, validatePositive};
+    printf("---- Buscar Agendamento ----\n");
+    readStrField(id, "Código do Agendamento", 6, idRules, 3);
     printf("------------------------------------------------------------------\n");
-    printf("ID: %d\nCódigo Cliente: %d\nCódigo Advogado: %d\nCódigo Escritório: %d\nData: %s\nHorário: %s\n", id, 1, 1, 1, "", "");
+    printf("ID: %s\nCódigo Cliente: %d\nCódigo Advogado: %d\nCódigo Escritório: %d\nData: %s\nHorário: %s\n", id, 1, 1, 1, "", "");
     printf("------------------------------------------------------------------\n");
     printf("Pressione <Enter> para prosseguir...\n");
     proceed();
@@ -95,30 +89,19 @@ void readAppointment() {
  */
 void updateAppointment() {
     Appointment appointment;
-    char date[11], startTime[6], endTime[6];
-    int id;
-
-    printf("---- Cadastrar Agendamento ----\nCódigo do Agendamento: ");
-    scanf("%d", &id);
-
-    printf("Código do Cliente: ");
-    scanf("%d", &appointment.clientId);
-
-    printf("Código do Advogado: ");
-    scanf("%d", &appointment.lawyerId);
-
-    printf("Código do Escritório: ");
-    scanf("%d", &appointment.officeId);
-    flushInput();
-
-    printf("Data (dd/mm/aaaa): ");
-    readline(date, 11);
-
-    printf("Horário do início da consulta (hh:mm): ");
-    readline(startTime, 11);
-
-    printf("Horário do término da consulta (hh:mm): ");
-    readline(endTime, 11);
+    char date[11], startTime[6], endTime[6], appointmentId[6], clientId[6], lawyerId[6], officeId[6];
+    Validation idRules[3] = {validateRequired, validateNumber, validatePositive},
+        dateRules[2] = {validateRequired, validateDate},
+        hourRules[2] = {validateRequired, validateHour};
+    
+    printf("---- Atualizar Agendamento ----\n");
+    readStrField(appointmentId, "Código do Agendamento", 6, idRules, 3);
+    readStrField(clientId, "Código do Cliente", 6, idRules, 3);
+    readStrField(lawyerId, "Código do Advogado", 6, idRules, 3);
+    readStrField(officeId, "Código do Escritório", 6, idRules, 3);
+    readStrField(date, "Data (dd/mm/aaaa)", 11, dateRules, 2);
+    readStrField(startTime, "Horário do início da consulta (hh:mm)", 6, hourRules, 2);
+    readStrField(endTime, "Horário do término da consulta (hh:mm)", 6, hourRules, 2);
 
     printf("\nAgendamento editado com sucesso!\nPressione <Enter> para prosseguir...\n");
     proceed();
@@ -133,9 +116,10 @@ void updateAppointment() {
  *  - https://github.com/akemi-adam
  */
 void deleteAppointment() {
-    int id;
-    printf("---- Deletar Agendamento ----\nCódigo do Agendamento: ");
-    scanf("%d", &id);
+    char id[6];
+    Validation idRules[3] = {validateRequired, validateNumber, validatePositive};
+    printf("---- Deletar Agendamento ----\n");
+    readStrField(id, "Código do Agendamento", 6, idRules, 3);
     printf("Agendamento deletado com sucesso!\nPressione <Enter> para prosseguir...\n");
     proceed();
 }
