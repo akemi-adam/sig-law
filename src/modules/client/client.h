a5bdac8c89608eb40e640c50dc1eaa0bf63e8333
akemi-adam <bazilixo8@gmail.com>
2024-12-15 20:11:36 -0300

    new: Assinaturas das funções de ORM e campo de softdelete no módulo client

--- src/modules/client/client.h
+++ src/modules/client/client.h
@@ -1,11 +1,13 @@
 #ifndef CLIENT
 #define CLIENT
 
+#include <stdbool.h>
 #include "./../person/person.h"
 
 typedef struct Client {
     int id;
     Person person;
+    bool isDeleted;
 } Client;
 
 void showClientMenu(void);
@@ -19,5 +21,11 @@ void listClients(void);
 void updateClient(void);
 
 void deleteClient(void);
+
+Client* getClients(int*);
+
+Client* findClient(int);
+
+void editClients(int, Client*);
 
 #endif
