ece72174cf555ed4351c5507e73c674c99cd8619
veraxqy <elderbruno2014@gmail.com>
2025-01-19 18:22:39 -0300

    new: Criação do modulo relatórios

---
+++ src/modules/report/report.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include "./../../utils/interfaces.h"
+#include "./../../utils/validation.h"
+#include "./../../utils/storage.h"
+#include "./../../utils/date.h"
+#include "./../../utils/str.h"
+#include "./report.h"
+#include "../client/client.h"
+#include "../lawyer/lawyer.h"
+#include "../office/office.h"
+#include "../appointment/appointment.h"
+#include "../person/person.h"
+
+#ifdef __unix__
+
+#include <termios.h>
+#include <unistd.h>
+
+
+/**
+ * Relatórios
+ * 
+ * @return void
+ * 
+ * Authors:
+ *  - https://github.com/akemi-adam
+ */
+
+#endif
---
+++ src/modules/report/report.h
@@ -0,0 +1,6 @@
+#ifndef REPORT_H
+#define REPORT_H
+
+#include <stdio.h>
+
+#endif
